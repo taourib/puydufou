9b3b36282948e6b2d3f985bcbd3d20296f7dac2a
xarux49 <147587445+xarux49@users.noreply.github.com>
2023-12-01 11:09:01 +0100

    Update

--- vues/v_champInscription.php
+++ vues/v_champInscription.php
@@ -4,22 +4,22 @@
 			<form action="index.php?uc=connexion&action=confirmInscription" method="post">
 				<p>
 					<label>Nom : </label>
-					<input type="text" name="nomClient">
+					<input type="text" name="nomClient" required>
 				</p>
 				<p>
 					<label>Prénom : </label>
-					<input type="text" name="prenomClient">
+					<input type="text" name="prenomClient" required>
 				</p>
 				<p>
 					<label>Mail : </label>
-					<input type="text" name="mailClient">
+					<input type="email" name="mailClient" required>
 				</p>
 				<p>
 				<p>
 					<label>Mot de passe : </label>
-					<input type="password" name="mdpClient">
+					<input type="password" name="mdpClient" required>
 				</p>
-				<input type="submit" name="Valider">
+				<input type="submit" value="Valider">
 			</form>
 		</fieldset>
 	</div>
@@ -28,20 +28,36 @@
 
 
 	<style>
-	#inscrire{
+		#inscrire {
 			display: flex;
 			justify-content: center;
 			align-items: center;
-			height: 100vh; /* Set the height to 100% of the viewport height */
+			height: 100vh;
 		}
 
-	legend{
-		font-size: 1.5rem;
-	}
+		legend {
+			font-size: 1.5rem;
+		}
 
-	form{
-		font-size: 1.4rem;
-		text-align: right;
-	}
+		form {
+			font-size: 1.4rem;
+			text-align: right;
+		}
+
+		input {
+			margin-bottom: 1%;
+		}
+
+		input[type="submit"] {
+			transition: background-color 0.3s, color 1.0s;
+			width: 25%;
+   			margin-right: 11%;
+
+		}
+
+		input[type="submit"]:hover {
+			background-color: red;
+			color: white;
+			/* Change text color on hover, adjust as needed */
+		}
 	</style>
-
