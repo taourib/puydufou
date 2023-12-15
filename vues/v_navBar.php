df134a65111c88bb2e37a65ca81f24d6dd0116a0
Leroux Théo <lerouxt@saintemarie-cholet.eu>
2023-12-15 11:01:27 +0100

    test

--- vues/v_navBar.php
+++ vues/v_navBar.php
@@ -1,52 +1,40 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
-<!--  TITRE ET MENUS -->
-<html lang="fr">
-<head>
-<title>PuyDuFou</title>
-<meta http-equiv="Content-Language" content="fr">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link href="styles/global.css" rel="stylesheet" type="text/css">
-</head>
-<body >
- <header>
-      <nav class="navbar">
-      	<ul class="navbar-nav">
-        	<a href="index.php?uc=accueil&action=accueilPage" class="icon"><img class="logo" src="images/logo.svg"></a>
-        		<?php if(empty($_SESSION)){ ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?uc=planning&action=viewPlanning">Planning</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?uc=connexion&action=connexion">Connexion</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?uc=connexion&action=inscription">Inscription</a>
-                    </li>
-                <?php }else{ ?>
-                <?php if($_SESSION['Id_profil']=="Administrateur"){ ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?uc=planning&action=viewPlanning">Planning</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?uc=profil&action=viewProfil">Profil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?uc=chemin&action=chemin">Chemin</a>
-                    </li>   
+<header>
+    <nav class="navbar">
+    <ul class="navbar-nav">
+        <a href="index.php?uc=accueil&action=accueilPage" class="icon"><img class="logo" src="images/logo.svg"></a>
+            <?php if(empty($_SESSION)){ ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?uc=planning&action=viewPlanning">Planning</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?uc=connexion&action=connexion">Connexion</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?uc=connexion&action=inscription">Inscription</a>
+                </li>
+            <?php }else{ ?>
+            <?php if($_SESSION['Id_profil']=="Administrateur"){ ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?uc=planning&action=viewPlanning">Planning</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?uc=profil&action=viewProfil">Profil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?uc=chemin&action=chemin">Chemin</a>
+                </li>   
 
-                    <?php }else{ ?>
-                        <li class="nav-item">
-                        <a class="nav-link" href="index.php?uc=planning&action=viewPlanning">Planning</a>
-                    </li>
+                <?php }else{ ?>
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php?uc=profil&action=viewProfil">Profil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?uc=Reservation&action=Reservation">Reservation</a>
-                    </li>   
-                <?php } ?>
-                    
+                    <a class="nav-link" href="index.php?uc=planning&action=viewPlanning">Planning</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?uc=profil&action=viewProfil">Profil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?uc=Reservation&action=Reservation">Reservation</a>
+                </li>   
+            <?php } ?>
         <?php } ?>
       	</ul>
     </nav>
